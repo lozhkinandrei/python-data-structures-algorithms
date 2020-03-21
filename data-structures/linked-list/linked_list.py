cc0f093aedc1b8dcac4919803496d5ee8d4d973d
Andrei Lozhkin <andreilozhkindev@gmail.com>
2020-03-21 16:15:32 +0700

    Add linked list

---
+++ data-structures/linked-list/linked_list.py
@@ -0,0 +1,103 @@
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self._size = 0
+
+    def add(self, value):
+        self.head = Node(value, self.head)
+        self._size += 1
+
+    def append(self, value):
+        node = self.head
+
+        if node:
+            while node.next:
+                prev = node
+                node = node.next
+            node.next = Node(value)
+        else:
+            self.head = Node(value)
+
+        self._size += 1
+
+    def search(self, value):
+        node = self.head
+        while node:
+            if node.value == value:
+                return True
+            node = node.next
+        return False
+
+    def remove(self, value):
+        if self.index(value):
+            node = self.head
+            while node:
+                if node.value == value:
+                    prev.next = node.next
+                    self._size -= 1
+                    break
+                prev = node
+                node = node.next
+
+    def index(self, value):
+        node = self.head
+        index = 0
+        while node:
+            if node.value == value:
+                return index
+            index += 1
+            node = node.next
+        raise ValueError('{} is not in list'.format(value))
+
+    def pop(self, pos=None):
+        node = self.head
+        index = 0
+
+        if node is None:
+            raise IndexError('pop from empty list')
+
+        if pos is not None:
+            if pos == 0:
+                self._size -= 1
+                value = self.head.value
+                self.head = self.head.next
+                return value
+
+            while node.next:
+                prev = node
+                node = node.next
+                index += 1
+                if index == pos:
+                    self._size -= 1
+                    prev.next = node.next
+                    return node.value
+            raise IndexError('pop from empty list')
+
+        while node.next:
+            prev = node
+            node = node.next
+        prev.next = None
+        self._size -= 1
+        return node.value
+
+    def size(self):
+        return self._size
+
+    def is_empty(self):
+        return self.head == None
+
+    def __str__(self):
+        result = ''
+        node = self.head
+        while node:
+            result += '({}) => '.format(node.value)
+            node = node.next
+        result += '(None)'
+        return result
---
+++ data-structures/linked-list/tests.py
@@ -0,0 +1,76 @@
+import unittest
+from linked_list import LinkedList
+
+
+class TestLinkedListMethods(unittest.TestCase):
+    def setUp(self):
+        self.list = LinkedList()
+
+    def test_add(self):
+        self.list.add(1)
+        self.assertEqual(self.list.index(1), 0)
+        self.list.add(2)
+        self.assertEqual(self.list.index(2), 0)
+        self.list.add(3)
+        self.assertEqual(self.list.index(3), 0)
+
+    def test_append(self):
+        self.list.append(1)
+        self.assertEqual(self.list.index(1), 0)
+        self.list.append(2)
+        self.assertEqual(self.list.index(2), 1)
+        self.list.append(3)
+        self.assertEqual(self.list.index(3), 2)
+
+    def test_search(self):
+        self.list.add(1)
+        self.list.add(2)
+        self.assertEqual(self.list.search(2), True)
+        self.assertEqual(self.list.search(3), False)
+
+    def test_remove(self):
+        with self.assertRaises(ValueError): self.list.remove(1)
+        self.list.add(1)
+        self.list.add(2)
+        self.list.add(3)
+        self.assertEqual(self.list.remove(2), None)
+        self.assertEqual(self.list.search(2), False)
+
+    def test_index(self):
+        with self.assertRaises(ValueError): self.list.index(1)
+        self.list.add(1)
+        self.list.add(2)
+        self.list.add(3)
+        self.assertEqual(self.list.index(3), 0)
+
+    def test_pop(self):
+        with self.assertRaises(IndexError): self.list.pop()
+        with self.assertRaises(IndexError): self.list.pop(1)
+        self.list.add(1)
+        self.list.add(2)
+        self.list.add(3)
+        self.list.add(4)
+        self.assertEqual(self.list.pop(), 1)
+        self.assertEqual(self.list.pop(1), 3)
+        self.assertEqual(self.list.size(), 2)
+
+    def test_size(self):
+        self.assertEqual(self.list.size(), 0)
+        self.list.add(1)
+        self.assertEqual(self.list.size(), 1)
+
+    def test_is_empty(self):
+        self.assertEqual(self.list.is_empty(), True)
+        self.list.add(1)
+        self.assertEqual(self.list.is_empty(), False)
+
+    def test__str__(self):
+        self.assertEqual(self.list.__str__(), '(None)')
+        self.list.add(1)
+        self.list.add(2)
+        self.list.add(3)
+        self.assertEqual(self.list.__str__(), '(3) => (2) => (1) => (None)')
+
+
+if __name__ == '__main__':
+    unittest.main()
