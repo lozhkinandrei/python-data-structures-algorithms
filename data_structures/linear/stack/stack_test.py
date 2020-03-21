ca59f00f1fb236222d736efa5f18618deafb66c2
Andrei Lozhkin <andreilozhkindev@gmail.com>
2020-03-21 23:59:55 +0700

    Add missing tests

---
+++ data_structures/linear/stack/stack_test.py
@@ -0,0 +1,38 @@
+import unittest
+from stack import Stack
+
+
+class TestStackMethods(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.items, [1])
+        self.stack.push(2)
+        self.assertEqual(self.stack.items, [1, 2])
+
+    def test_pop(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.items, [1])
+
+    def test_peek(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.peek(), 2)
+
+    def test_size(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.size(), 2)
+
+    def test_is_empty(self):
+        self.assertEqual(self.stack.is_empty(), True)
+        self.stack.push(1)
+        self.assertEqual(self.stack.is_empty(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
