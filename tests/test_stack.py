0e5508b23ba1a543660c6ed47c95291619bebff0
Andrei Lozhkin <andreilozhkindev@gmail.com>
2021-09-21 15:12:40 +0000

    test: refactor Stack tests

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -13,25 +13,28 @@ class TestStackMethods(unittest.TestCase):
         self.assertEqual(self.stack.items, [1, 2])
 
     def test_pop(self):
-        self.stack.push(1)
-        self.stack.push(2)
+        self.stack.items = [1, 2]
         self.assertEqual(self.stack.pop(), 2)
         self.assertEqual(self.stack.items, [1])
 
     def test_peek(self):
-        self.stack.push(1)
-        self.stack.push(2)
+        self.stack.items = [1, 2]
         self.assertEqual(self.stack.peek(), 2)
 
     def test_size(self):
-        self.stack.push(1)
-        self.stack.push(2)
+        self.stack.items = [1, 2]
         self.assertEqual(self.stack.size(), 2)
 
-    def test_is_empty(self):
+    def test_is_empty_true(self):
         self.assertEqual(self.stack.is_empty(), True)
-        self.stack.push(1)
+
+    def test_is_empty_false(self):
+        self.stack.items = [1, 2]
         self.assertEqual(self.stack.is_empty(), False)
+
+    def test_str(self):
+        self.stack.items = [1, 2, 3]
+        self.assertEqual(str(self.stack), "[1, 2, 3]")
 
 
 if __name__ == "__main__":
