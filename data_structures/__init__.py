31dc4be4c9007cb626be41a0c2caec00bdf02518
Andrei Lozhkin <andrei@DESKTOP-63NAMPR.localdomain>
2021-10-16 17:51:05 +0700

    refactor: remove unnecessary imports

--- data_structures/__init__.py
+++
@@ -1,5 +0,0 @@
-from .linear.deque import Deque
-from .linear.linked_list import LinkedList
-from .linear.queue import Queue
-from .linear.stack import Stack
-from .non_linear.tree import Node as Tree
